72f3badc1ee83022c2e80d2ec7632f74cd36f828
vidhan <vidhn@hotmail.com>
2025-01-09 01:04:17 +0530

    pydantic

--- books.py
+++ books.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -19,19 +19,19 @@ class Book:
 
 BOOKS = [
     Book(1, "My book 1", "Vidhan", "My first book", 5),
-    Book(2, "My book 2", "Vidhan", "My first book", 3),
-    Book(3, "My book 3", "Pandey", "My first book", 4),
-    Book(4, "My book 4", "Pandey", "My first book", 4),
-    Book(5, "My book 5", "Vidhan", "My first book", 5),
-    Book(6, "My book 6", "Vidhan", "My first book", 5),
+    Book(2, "My book 2", "Vidhan", "My second book", 3),
+    Book(3, "My book 3", "Pandey", "My third book", 4),
+    Book(4, "My book 4", "Pandey", "My fourth book", 4),
+    Book(5, "My book 5", "Vidhan", "My fifth book", 5),
+    Book(6, "My book 6", "Vidhan", "My sixth book", 5),
 ]
 
 class BookRequest(BaseModel):
     id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=6)
 
 @app.get("/books")
 def read_all_books():
